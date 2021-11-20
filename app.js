1c9453f13a3ee6fafe2342a2a6d9b82e66029fab
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2021-11-21 04:21:34 +0530

    added all new styles in all elements

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const blogs = require('./routes/blog')
 
 // set port number or 
@@ -8,17 +9,23 @@ const port = 3000
 // set view engine
 app.set('view engine', 'ejs')
 
-// prefix for blogs route
-const prefix = 'blogs'
-
 // middleware
 app.use(express.static('public'))
 app.use(express.static('public/projects'))
-app.use(`/${prefix}`, blogs)
+app.use('/', blogs)
 
 
 app.get('/', (req, res) => {
-	res.render('index.ejs')
+	axios.get('https://dev.to/api/articles?username=abhidadhaniya23')
+		.then(response => {
+			// console.log(response.data);
+			res.render('index.ejs', {
+				articles: response.data
+			})
+		}).catch(err => {
+			console.log(err);
+			// res.render('error page');
+		})
 })
 
 app.listen(port, () => {
--- routes/blog.js
+++ routes/blog.js
@@ -1,15 +1,89 @@
+require('dotenv').config()
 const blogs = require('express').Router()
 const axios = require('axios')
+var md = require('markdown-it')();
+const fs = require('fs')
+const path = require('path')
 
-// https://dev.to/api/articles/me/published
+const URL = 'https://dev.to/api/articles/me/published'
 
-axios.get('https://jsonplaceholder.typicode.com/todos/1')
+let userData
+
+axios.get('https://dev.to/api/articles?username=abhidadhaniya23')
     .then(response => {
-        console.log(response.data);
+        userData = response.data
     });
 
-blogs.get('/:post', (req, res) => {
-    res.send('Hello world')
+const options = {
+    headers: { 'api-key': process.env.API_KEY },
+    params: { 'per_page': 10 }
+}
+
+function timeSince(date) {
+    let seconds = Math.floor((new Date() - date) / 1000);
+    let interval = seconds / 31536000;
+    if (interval > 1) {
+        return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
+
+blogs.get('/:title', (req, res) => {
+
+    try {
+        let post = userData.find(post => post.title.replace(/\s/g, '-') == req.params.title.replace(/\s/g, '-'))
+        // console.log(post.title);
+        axios.get(URL, options)
+            .then(response => {
+                response.data.forEach(article => {
+                    if (article.title == post.title) {
+                        const articleContent = md.render(article.body_markdown.replace(/\\n/g, '\n'))
+                        fs.writeFile('partials/postContent.ejs', articleContent, err => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            var myDate = new Date(article.published_at);
+                            var result = myDate.getTime(myDate);
+                            res.render('article.ejs', {
+                                title: article.title,
+                                // content: articleContent,
+                                createdTime: timeSince(result),
+                                readingTime: article.reading_time_minutes,
+                                coverImg: article.cover_image
+                            })
+                            // console.log('Successfully Done!');
+                        })
+                    }
+                })
+            })
+            .catch(error => {
+                console.log('ERROR: ', error)
+            })
+    } catch (error) {
+        console.log(error);
+    }
+
+    // const post = article.body_markdown
+    // .replace(/\s+/g, '-')
+    // .replace(/[^a-zA-Z0-9-]/g, '')
+    // .toLowerCase()
+    // console.log(post);
 });
 
 module.exports = blogs
