710458d7e687ad8589bb4d1090bcb4fbde16056e
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2021-11-29 21:03:03 +0530

    repair some broken links

--- public/projects/Common-Js/projects.js
+++ public/projects/Common-Js/projects.js
@@ -10,47 +10,47 @@ let projectsData = [
     },
     {
         'projectName': 'Rock-Paper-Scissor Game',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/RPS-Game/index.html'
+        'hrefLocation': '/RPS-Game/index.html'
     },
     {
         'projectName': 'Countdown Timer',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/countdown-timer/index.html'
+        'hrefLocation': '/countdown-timer/index.html'
     },
     {
         'projectName': 'Time Square News',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/time-square/index.html'
+        'hrefLocation': '/time-square/index.html'
     },
     {
         'projectName': 'Secure Password Generator',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/Secure-password-generator/index.html'
+        'hrefLocation': '/Secure-password-generator/index.html'
     },
     {
         'projectName': 'Covid-19 Tracker',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/Covid-19-Tracker/index.html'
+        'hrefLocation': '/Covid-19-Tracker/index.html'
     },
     {
         'projectName': 'Periodic Table',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/Periodic-table/index.html'
+        'hrefLocation': '/Periodic-table/index.html'
     },
     {
         'projectName': 'Color-Game',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/Color-game/index.html'
+        'hrefLocation': '/Color-game/index.html'
     },
     {
         'projectName': 'Quiz App',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/Quiz-app/index.html'
+        'hrefLocation': '/Quiz-app/index.html'
     },
     {
         'projectName': 'Food Demo Site',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/Food-site/index.html'
+        'hrefLocation': '/Food-site/index.html'
     },
     {
         'projectName': 'Photo Finder',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/Photos-finder/index.html'
+        'hrefLocation': '/Photos-finder/index.html'
     },
     {
         'projectName': 'Quotes-for-life',
-        'hrefLocation': 'https://www.web-developer-abhi.tk/Quotes-for-life/index.html'
+        'hrefLocation': '/Quotes-for-life/index.html'
     }
 ];
 
