9cf4f1cbf3b8d4c22828a7a2fb26ad893533d19d
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-02-01 12:02:38 +0530

    now 2 blogs are only appear

--- app.js
+++ app.js
@@ -1,64 +1,66 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const axios = require('axios')
-const bodyParser = require('body-parser')
-const nodemailer = require('nodemailer')
-const blogs = require('./routes/blog')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const blogs = require("./routes/blog");
 
-// set port number or 
-const port = process.env.PORT || 3000
+// set port number or
+const port = process.env.PORT || 3000;
 
 // set view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // middleware
-app.use(express.static('public'))
-app.use(express.static('public/projects'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/', blogs)
+app.use(express.static("public"));
+app.use(express.static("public/projects"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", blogs);
 
 // mail-sender
 const transporter = nodemailer.createTransport({
-	host: "smtp.mailtrap.io",
-	port: 2525,
-	auth: {
-		user: process.env.USER,
-		pass: process.env.PASS
-	}
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
 });
 
-app.get('/', (req, res) => {
-	axios.get('https://dev.to/api/articles?username=abhidadhaniya23')
-		.then(response => {
-			// console.log(response.data);
-			res.render('index.ejs', {
-				articles: response.data
-			})
-		}).catch(err => {
-			console.log(err);
-			// res.render('error page');
-		})
-})
+app.get("/", (req, res) => {
+  axios
+    .get("https://dev.to/api/articles?username=abhidadhaniya23")
+    .then((response) => {
+      //   console.log(response.data);
+      res.render("index.ejs", {
+        articles: response.data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      // res.render('error page');
+    });
+});
 
-app.post('/', (req, res) => {
-	// console.log(`Name : ${req.body.name} <br> Email : ${req.body.email} <br> Message : ${req.body.message}`);
-	const mailOptions = {
-		from: `${req.body.email}`,
-		to: 'abhidadhaniya23@gmail.com',
-		subject: `Email by ${req.body.name}`,
-		html: `Name : ${req.body.name} <br> Email : ${req.body.email} <br> Message : ${req.body.message}`
-	}
-	transporter.sendMail(mailOptions, function (error, info) {
-		if (error) {
-			console.log(error);
-		} else {
-			// console.log('Email sent: ' + info.response);
-			res.render('thankyou.ejs', { name: req.body.name })
-		}
-	});
-})
+app.post("/", (req, res) => {
+  // console.log(`Name : ${req.body.name} <br> Email : ${req.body.email} <br> Message : ${req.body.message}`);
+  const mailOptions = {
+    from: `${req.body.email}`,
+    to: "abhidadhaniya23@gmail.com",
+    subject: `Email by ${req.body.name}`,
+    html: `Name : ${req.body.name} <br> Email : ${req.body.email} <br> Message : ${req.body.message}`,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      // console.log('Email sent: ' + info.response);
+      res.render("thankyou.ejs", { name: req.body.name });
+    }
+  });
+});
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- routes/blog.js
+++ routes/blog.js
@@ -1,84 +1,82 @@
-require('dotenv').config()
-const blogs = require('express').Router()
-const axios = require('axios')
-var md = require('markdown-it')();
-const fs = require('fs')
-const path = require('path')
+require("dotenv").config();
+const blogs = require("express").Router();
+const axios = require("axios");
+var md = require("markdown-it")();
+const fs = require("fs");
+const path = require("path");
 
-const URL = 'https://dev.to/api/articles/me/published'
+const URL = "https://dev.to/api/articles/me/published";
 
 const options = {
-    headers: { 'api-key': process.env.API_KEY },
-    params: { 'per_page': 10 }
-}
+  headers: { "api-key": process.env.API_KEY },
+  params: { per_page: 10 },
+};
 
 function timeSince(date) {
-    let seconds = Math.floor((new Date() - date) / 1000);
-    let interval = seconds / 31536000;
-    if (interval > 1) {
-        return Math.floor(interval) + " years";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-        return Math.floor(interval) + " months";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-        return Math.floor(interval) + " days";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-        return Math.floor(interval) + " hours";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-        return Math.floor(interval) + " minutes";
-    }
-    return Math.floor(seconds) + " seconds";
+  let seconds = Math.floor((new Date() - date) / 1000);
+  let interval = seconds / 31536000;
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
 }
 
-blogs.get('/:title', (req, res) => {
+blogs.get("/:title", (req, res) => {
+  try {
+    axios.get("https://dev.to/api/articles?username=abhidadhaniya23").then((response) => {
+      userData = response.data;
+      let post = userData.find((post) => post.title.replace(/\s/g, "-") === req.params.title.replace(/\s/g, "-"));
+      // console.log(post);
+      axios
+        .get(URL, options)
+        .then((response) => {
+          response.data.forEach((article) => {
+            if (article.title == post.title) {
+              const articleContent = md.render(article.body_markdown.replace(/\\n/g, "\n"));
+              let myDate = new Date(article.published_at);
+              let result = myDate.getTime(myDate);
 
-    try {
-        axios.get('https://dev.to/api/articles?username=abhidadhaniya23')
-            .then(response => {
-                userData = response.data
-                let post = userData.find(post => post.title.replace(/\s/g, '-') === req.params.title.replace(/\s/g, '-'))
-                // console.log(post);
-                axios.get(URL, options)
-                    .then(response => {
-                        response.data.forEach(article => {
-                            if (article.title == post.title) {
+              res.render("article.ejs", {
+                title: article.title,
+                content: articleContent,
+                createdTime: timeSince(result),
+                readingTime: article.reading_time_minutes,
+                coverImg: article.cover_image,
+              });
 
-                                const articleContent = md.render(article.body_markdown.replace(/\\n/g, '\n'))
-                                let myDate = new Date(article.published_at);
-                                let result = myDate.getTime(myDate);
+              // fs.unlink(path.join(__dirname, '../partials/postContent.ejs'), err => { console.log(err); })
+            }
+          });
+        })
+        .catch((error) => {
+          console.log("ERROR: ", error);
+        });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-                                res.render('article.ejs', {
-                                    title: article.title,
-                                    content: articleContent,
-                                    createdTime: timeSince(result),
-                                    readingTime: article.reading_time_minutes,
-                                    coverImg: article.cover_image
-                                })
-
-                                // fs.unlink(path.join(__dirname, '../partials/postContent.ejs'), err => { console.log(err); })
-                            }
-                        })
-                    })
-                    .catch(error => {
-                        console.log('ERROR: ', error)
-                    })
-            });
-    } catch (error) {
-        console.log(error);
-    }
-
-    // const post = article.body_markdown
-    // .replace(/\s+/g, '-')
-    // .replace(/[^a-zA-Z0-9-]/g, '')
-    // .toLowerCase()
-    // console.log(post);
+  // const post = article.body_markdown
+  // .replace(/\s+/g, '-')
+  // .replace(/[^a-zA-Z0-9-]/g, '')
+  // .toLowerCase()
+  // console.log(post);
 });
 
-module.exports = blogs
+module.exports = blogs;
